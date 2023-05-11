d045123bc58a225d7d67e6a19c4d51cd40c9c9fa
james-love <27236856+james-love@users.noreply.github.com>
2023-05-11 17:36:24 -0500

    boss push maybe

--- UnityGame/Assets/UI Toolkit/ScoreScreen/ScoreScreen.cs
+++ UnityGame/Assets/UI Toolkit/ScoreScreen/ScoreScreen.cs
@@ -1,24 +1,53 @@
 using HighScore;
+using StarterAssets;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class ScoreScreen : MonoBehaviour
 {
     private readonly string placeHolderText = "Input name...";
     private VisualElement root;
+    private float startTime;
+
+    public void Open()
+    {
+        Time.timeScale = 0f;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInput>().currentActionMap.Disable();
+        UpdateDisplay();
+        root.style.display = DisplayStyle.Flex;
+    }
 
     private void Awake()
     {
-        Time.timeScale = 0;
-        HS.Init(this, "Team04Game");
+        HS.Init(this, "Ascent into Madness");
+        startTime = Time.time;
 
         root = GetComponent<UIDocument>().rootVisualElement;
+
+        root.style.display = DisplayStyle.None;
 
         TextField name = root.Q<TextField>("NameInput");
         name.SetPlaceholderText(placeHolderText);
 
         Button submitButton = root.Q<Button>("SubmitButton");
         submitButton.clicked += SubmitScore;
+    }
+
+    private void UpdateDisplay()
+    {
+        int elapsedTime = Mathf.FloorToInt(Time.time - startTime);
+        int minutes = elapsedTime / 60;
+        int seconds = elapsedTime % 60;
+
+        int deaths = 0;
+        int tomes = GameObject.FindGameObjectWithTag("Player").GetComponent<ThirdPersonController>().tomesCollected;
+        int maxTomes = 8;
+
+        root.Q<Label>("TimeValue").text = $"{minutes}:{seconds}";
+        root.Q<Label>("TomesValue").text = $"{tomes}/{maxTomes}";
+        root.Q<Label>("DeathsValue").text = deaths.ToString();
+        root.Q<Label>("FinalValue").text = Mathf.Clamp(20_000 - (deaths * 100) - (elapsedTime * 10) - ((maxTomes - tomes) * 25), 0, 20_000).ToString();
     }
 
     private void SubmitScore()
